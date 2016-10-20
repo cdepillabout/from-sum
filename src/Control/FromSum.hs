e26fa0d724f4b605d1651daf2cd4f45eadba0022
(cdep)illabout <cdep.illabout@gmail.com>
2016-10-20 16:51:17 +0900

    Added 'Or' functions and also fixed some documentation.

--- src/Control/FromSum.hs
+++ src/Control/FromSum.hs
@@ -14,10 +14,14 @@ This Haskell module exports various \"from\" functions for 'Either' and
 module Control.FromSum
   ( -- * Monadic in return value
     fromEitherM
+  , fromEitherOrM
   , fromMaybeM
+  , fromMaybeOrM
     -- * Monadic in both return and sum-type value
   , fromEitherMM
+  , fromEitherOrMM
   , fromMaybeMM
+  , fromMaybeOrMM
     -- * Completely non-monadic functions
   , fromEither
   , fromMaybe
@@ -44,6 +48,23 @@ fromEitherM
   => (e -> m a) -> Either e a -> m a
 fromEitherM leftAction = either leftAction pure
 
+-- | A 'flip'ed version of 'fromEitherM'.
+--
+-- >>> fromEitherOrM (Right 5) $ \s -> [length s]
+-- [5]
+--
+-- This can be nice to use as an error handler.
+--
+-- >>> fromEitherOrM (Right 5) $ \s -> putStrLn ("error: " ++ s) >> undefined
+-- 5
+-- >>> fromEitherOrM (Left "foo") $ \s -> putStrLn ("error: " ++ s) >> undefined
+-- error: foo
+-- ...
+fromEitherOrM
+  :: Applicative m
+  => Either e a -> (e -> m a) -> m a
+fromEitherOrM = flip fromEitherM
+
 -- | A monadic version of 'fromMaybe'.
 --
 -- @
@@ -59,12 +80,29 @@ fromMaybeM
   => m a -> Maybe a -> m a
 fromMaybeM nothingAction = maybe nothingAction pure
 
--- | Similar to 'fromEitherM' but 'Either' argument is also a monadic value.
+-- | A 'flip'ed version of 'fromMaybeM'.
 --
--- >>> fromEitherMM (\s -> [length s]) . pure $ Right 5
+-- >>> fromMaybeOrM (Just 5) []
 -- [5]
--- >>> fromEitherMM (\s -> [length s]) . pure $ Left ("foo" :: String)
--- [3]
+--
+-- This can be nice to use as an error handler.
+--
+-- >>> fromMaybeOrM (Just 5) $ putStrLn "some error occurred" >> undefined
+-- 5
+-- >>> fromMaybeOrM (Nothing) $ putStrLn "some error occurred" >> undefined
+-- some error occurred
+-- ...
+fromMaybeOrM
+  :: Applicative m
+  => Maybe a -> m a -> m a
+fromMaybeOrM = flip fromMaybeM
+
+-- | Similar to 'fromEitherM' but the 'Either' argument is also a monadic value.
+--
+-- >>> fromEitherMM (\s -> [length s]) [Right 5, Right 10]
+-- [5,10]
+-- >>> fromEitherMM (\s -> [length s]) [Left ("foo" :: String), Right 100]
+-- [3,100]
 --
 -- __NOTE__: I don't particularly like the name of this function.  If you have a
 -- suggestion for a better name, please submit a PR or issue.
@@ -73,12 +111,18 @@ fromEitherMM
   => (e -> m a) -> m (Either e a) -> m a
 fromEitherMM eitherAction mEither = fromEitherM eitherAction =<< mEither
 
--- | Similar to 'fromMaybeMA monadic version of 'fromMaybe'.
+-- | A 'flip'ed version of 'fromEitherMM'.
+fromEitherOrMM
+  :: Monad m
+  => m (Either e a) -> (e -> m a) -> m a
+fromEitherOrMM = flip fromEitherMM
+
+-- | Similar to 'fromMaybeM' but the 'Maybe' argument is also a monadic value.
 --
--- >>> fromMaybeMM [] . pure $ Just 5
--- [5]
--- >>> fromMaybeMM [] $ pure Nothing
--- []
+-- >>> fromMaybeMM [] [Just 6, Just 5]
+-- [6,5]
+-- >>> fromMaybeMM [] [Just 6, Nothing, Just 7]
+-- [6,7]
 --
 -- __NOTE__: I don't particularly like the name of this function.  If you have a
 -- suggestion for a better name, please submit a PR or issue.
@@ -86,6 +130,12 @@ fromMaybeMM
   :: Monad m
   => m a -> m (Maybe a) -> m a
 fromMaybeMM nothingAction mMaybe = fromMaybeM nothingAction =<< mMaybe
+
+-- | A 'flip'ed version of 'fromMaybeMM'.
+fromMaybeOrMM
+  :: Monad m
+  => m (Maybe a) -> m a -> m a
+fromMaybeOrMM = flip fromMaybeMM
 
 -- | Similar to 'fromMaybe'.
 --
